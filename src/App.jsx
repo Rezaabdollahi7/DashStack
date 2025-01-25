878d4fe4d695b53cab4dc330f26a9c3c9a026ffb
Rezaabdollahi7 <srezaabdollahi7@gmail.com>
2025-01-25 21:39:06 +0330

    build(deps): install core dependencies
    - Add React, Tailwind CSS, and Material-UI as project dependencies
    - Configure Tailwind CSS with vite
    - Set up Prettier with Tailwind plugin for code formatting
    - Add Prettier configuration file (.prettierrc)

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from '@mui/material'
+
+export default function App() {
+  return (
+    <div className='p-4'>
+      <h1 className='text-3xl font-bold text-blue-500'>Hello World </h1>
+      <Button variant='contained' color='primary'>
+        Test Btn
+      </Button>
+    </div>
+  )
+}
