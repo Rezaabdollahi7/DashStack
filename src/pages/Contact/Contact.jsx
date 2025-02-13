70a8f61b5bab1890561f30fca5de776996f6fd18
Rezaabdollahi7 <srezaabdollahi7@gmail.com>
2025-02-13 16:07:39 +0330

    feat (contacts List): add contacts section

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,3 +1,16 @@
+import { Contacts } from '../../constants/Contacts'
+import HeroTitle from '../../components/common/HeroTitle'
+import ContactCard from './ContactCard'
+
 export default function Contact() {
-  return <div>Contact</div>
+  return (
+    <>
+      <HeroTitle name='Contacts' />
+      <section className='contacts mt-6 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3'>
+        {Contacts.map((contact) => (
+          <ContactCard key={contact.id} contact={contact} />
+        ))}
+      </section>
+    </>
+  )
 }
