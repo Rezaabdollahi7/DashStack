e6250f3c79d64a86e6cf9ab8c252be03bde958a8
Rezaabdollahi7 <srezaabdollahi7@gmail.com>
2025-03-10 01:56:58 +0330

    fix : fix last visited height & dashboard between gap
    - change button styles in lastVisited section .
    - delete additional gap in top of home page ( header Underline ) .

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,7 +8,7 @@ import slides from '../../constants/DashboardCarouselSlides'
 import BestSalesman from './BestSalesman'
 export default function Home() {
   return (
-    <section className='flex flex-col gap-y-10'>
+    <section className='flex flex-col space-y-10 '>
       <HeroTitle name='Dashboard' />
       <section id='banner' className='grid grid-cols-12 gap-5'>
         <div className='banner-bg col-span-12 flex flex-col items-center justify-between gap-6 rounded-2xl px-8 py-14 lg:col-span-8 lg:flex-row'>
