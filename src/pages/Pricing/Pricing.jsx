879c67cdbec54c3c8def94a1bd08b635a4d75b7e
Rezaabdollahi7 <srezaabdollahi7@gmail.com>
2025-04-07 10:16:46 +0330

    core ( pricing / pricingCard ) : improve card sizes , remove additional scroll - decrease font sizes , spaces . - responsive gaps in md , lg , xl size.

--- src/pages/Pricing/Pricing.jsx
+++ src/pages/Pricing/Pricing.jsx
@@ -4,8 +4,8 @@ import PricingCart from './PricingCart'
 export default function Pricing() {
   return (
     <>
-      <HeroTitle name='pricing' />
-      <section className='my-6 grid grid-cols-1 gap-12 lg:grid-cols-3'>
+      <HeroTitle name='Pricing' />
+      <section className='grid grid-cols-1 gap-y-8 sm:grid-cols-2 sm:gap-x-12 lg:grid-cols-3 lg:gap-x-6 xl:gap-x-24'>
         {pricingPlans.map((plan) => PricingCart({ plan }))}
       </section>
     </>
