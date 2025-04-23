0dc9e857d77d57ed67852fe22d98efe927ae3eed
Rezaabdollahi7 <srezaabdollahi7@gmail.com>
2025-04-23 22:20:25 +0330

    feat ( add back , edit buttons )

--- src/pages/Products/ProductDetails.jsx
+++ src/pages/Products/ProductDetails.jsx
@@ -6,6 +6,10 @@ import ProductsInformation from './ProductDetails/ProductsInformation'
 import ProductFeatures from './ProductDetails/ProductFeatures'
 import ProductTabs from './ProductDetails/ProductTabs'
 import { Helmet } from 'react-helmet-async'
+import { IoChevronBack } from 'react-icons/io5'
+import { MdEdit } from 'react-icons/md'
+import { Link } from 'react-router-dom'
+
 export default function ProductDetails() {
   const [products] = useState(products_data)
   let selectedProduct = useParams()
@@ -27,7 +31,26 @@ export default function ProductDetails() {
           content='Product Details , name , price , images , buy '
         />
       </Helmet>
-      <section className='mx-auto grid grid-cols-12 px-2 py-7 sm:px-6 md:px-0 lg:gap-8 lg:px-12 xl:max-w-6xl'>
+      <section className='mx-auto grid grid-cols-12 px-2 py-7 sm:px-6 md:px-0 lg:gap-8 lg:px-12 xl:max-w-6xl xl:gap-12'>
+        {/* buttons section */}
+        <div className='col-span-12 flex items-center justify-between text-xs md:text-sm'>
+          <Link
+            to={'/products'}
+            className='flex items-center justify-center gap-2 rounded-lg bg-white px-2 py-2 font-semibold shadow-2xl'
+          >
+            <IoChevronBack className='size-4' />
+            <span>Back</span>
+          </Link>
+
+          <Link
+            to={`/product/${selectedProductsDetails.id}/edit`}
+            state={{ product: selectedProductsDetails }}
+            className='flex cursor-pointer items-start justify-center gap-2 rounded-lg bg-gray-950 px-4 py-2 font-semibold text-gray-200'
+          >
+            <span>Edit</span>
+            <MdEdit className='size-4' />
+          </Link>
+        </div>
         {/* left section */}
         <div className='left-section col-span-12 flex flex-col gap-4 lg:col-span-6'>
           <ProductSlider
